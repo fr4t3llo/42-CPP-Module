c57fc0b5fbf955acafbda2f4e83ba389ad4ecd30
saifeddinekasmi <saife.addine123@gmail.com>
2022-12-01 23:01:11 +0100

    add ex00

---
+++ cpp04/ex00/Animal.hpp
@@ -0,0 +1,16 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+
+class Animal 
+{
+    protected:
+        std::string type;
+    public:
+        Animal();
+        ~Animal();
+}
+
+#endif
