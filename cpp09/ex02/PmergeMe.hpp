7c11fa95c708e48379be579459b5b3145f047cc1
saifeddinekasmi <saife.addine123@gmail.com>
2023-03-19 18:19:11 +0000

    add ex03 but not complete

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,8 @@
+# ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <>
+
+
+#endif
---
+++ cpp09/ex02/main.cpp
@@ -0,0 +1,13 @@
+#include "PmergeMe.hpp"
+
+
+int main(int ac, char **av)
+{
+    if (ac == 2)
+    {
+
+    }
+    else
+        std::cout << "Error arg !" << std::endl;
+    return (0);
+}
